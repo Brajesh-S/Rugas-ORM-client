4cb766ee58a36d2befc0e77c772bc2fba21f4e49
Brajesh-S <Brajesh-S@users.noreply.github.com>
2025-03-19 21:43:59 +0530

    nitpicks

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -5,7 +5,7 @@ const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
 
   if (loading) return <div>Loading...</div>;
-  return user ? children : <Navigate to="/login" replace />;
+  return user ? children : <Navigate to="/dashboard" replace />;
 };
 
 export default ProtectedRoute;
